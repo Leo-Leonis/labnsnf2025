0fb54db30b3983edce01b27027ffcac06ba81d1f
Leo-Leonis <yeleandro@icloud.com>
2025-07-21 04:40:41 +0200

    fixed calibration params; added new histogram for p3; added more event cases, with in depth case management

--- Lab2hist.cpp
+++ Lab2hist.cpp
@@ -7,18 +7,33 @@
 #include "TCanvas.h"
 #include "TH1D.h"
 #include "TLegend.h"
+#include "TMath.h"
 #include "TStyle.h"
 
 #include <fstream>
 #include <iostream>
 
-void Lab2hist2(int filepath_option = 0, int do_debug = false) {
+// (LEO) Compares two ints from p1 and p2 and returns the smaller one
+// int SmallerOfTwo(int a, int b) {
+//   if (a < b)
+//     return a;
+//   else
+//     return b;
+// }
+
+void Lab2hist2(int filepath_option = 0, bool do_debug = false) {
+
+  gStyle->SetOptStat(111111);
 
   // (only in debug mode) decides the number of non triple-fff events being
   // recorded and in all these cases the p_n values are printed in the terminal
-  const int debug_detections = 20;
-
-  const int max_pn_value = 4095;
+  const int debug_detections = 1000;
+  // maximum value of each pn count
+  const int max_pn = 4095;
+  // max difference between coincident counts (events in which there are more
+  // than 2 positive stop signals)
+  const int max_diff = 10;
+  const int n_bins = 150;
 
   if (do_debug == 0)
     std::cout << "LEO_INFO: debug mode (=stop at 20 events) not activated"
@@ -52,45 +67,110 @@ void Lab2hist2(int filepath_option = 0, int do_debug = false) {
     break;
   }
 
-  // cal[0] = intercept, param[1] = slope
-  double const p1_cal[2] = {40.6223, 0.999062};
-  double const p2_cal[2] = {40.9588, 0.999077};
-  double const p3_cal[2] = {40.2021, 0.999524};
+  // cal[0] = intercept, cal[1] = slope
+  double const p1_cal[2] = {10.1525, 0.249765};
+  double const p2_cal[2] = {10.7402, 0.249769};
+  double const p3_cal[2] = {11.8005, 0.249881};
 
   // main histogram
-  auto *joint_hist = new TH1D(
-      "Joint histogram", "Detected events;Stop time (ns);Entries", 50, 0, 5000);
+  auto *joint_hist =
+      new TH1D("Joint histogram", "All TP stops;Stop time (ns);Entries", n_bins,
+               0, 16500);
   joint_hist->SetFillColor(kBlue);
+  auto *p3_hist = new TH1D("p3 stops", "PL3 stops;Stop time (ns);Entries",
+                           n_bins, 0, 16500);
+  p3_hist->SetFillColor(kRed);
 
   int ev, p1, p2, p3;
   double t;
   int n_detections = 0; // number of detections
-  int n_events[3] = {0, 0, 0};
+  int ev_type_count[8] = {0, 0, 0, 0,
+                          0, 0, 0, 0}; // all different cases except triple-fff
+
+  /* the cases are: ("FP" = false positive, "TN" true negative)
+        0. no no no (TN)
+        1. yes no no (FP)
+        2. no yes no (true positive)
+        3. no no yes (true positive)
+        4. yes yes no (true positive)
+        5. yes no yes (FP)
+        6. no yes yes (FP)
+        7. yes yes yes (FP)
+                                8. yes yes no, but p1 < p2 (FP)
+  */
+
   // integer that detects any kind of different case other than triple fff
-  int any_update;
+  // bool any_update = false;
 
   std::ifstream file(filepath_string.c_str());
-  while (!file.eof()) {
 
+  while (!file.eof()) {
     // read in the values
     file >> ev >> t >> p1 >> p2 >> p3;
 
-    // TODO: maybe add error argument
-    if (p1 < max_pn_value) {
-      joint_hist->Fill((p1 + p1_cal[0]) * 4 / p1_cal[1]);
-      any_update++;
-      n_events[0]++;
-    } else if (p2 < max_pn_value) {
-      joint_hist->Fill((p2 + p2_cal[0]) * 4 / p2_cal[1]);
-      any_update++;
-      n_events[1]++;
-    } else if (p3 < max_pn_value) {
-      joint_hist->Fill((p3 + p3_cal[0]) * 4 / p3_cal[1]);
-      any_update++;
-      n_events[2]++;
+    if (p1 + p2 + p3 == 12285) {
+      ev_type_count[0]++;
+      continue; // if no stop then skip
     }
 
-    if ((do_debug == 1) && (any_update != 0)) { // log the info with any update
+    if (p1 != max_pn) { // "pn != max_pn" is "yes", otherwise "no"
+      if (p2 != max_pn) {
+        if (p3 != max_pn) {
+          ev_type_count[7]++; // yes yes yes
+        } else {
+          if ((p1 >= p2) && ((p1 - p2) < max_diff)) {
+            ev_type_count[4]++; // yes yes no (TP)
+            joint_hist->Fill((static_cast<double>(p2) + p2_cal[0]) / p2_cal[1]);
+          }
+        }
+      } else {
+        if (p3 != max_pn) {
+          ev_type_count[5]++; // yes no yes
+        } else {
+          ev_type_count[1]++; // yes no no
+        }
+      }
+    } else {
+      if (p2 != max_pn) {
+        if (p3 != max_pn) {
+          ev_type_count[6]++; // no yes yes
+        } else {
+          ev_type_count[2]++; // no yes no (TP)
+          joint_hist->Fill((static_cast<double>(p2) + p2_cal[0]) / p2_cal[1]);
+        }
+      } else {
+        if (p3 != max_pn) {
+          ev_type_count[3]++; // no no yes (TP)
+          joint_hist->Fill((static_cast<double>(p3) + p3_cal[0]) / p3_cal[1]);
+          p3_hist->Fill((static_cast<double>(p3) + p3_cal[0]) / p3_cal[1]);
+        } else { // no no no (check case if all correct)
+          if (p1 + p2 + p3 == max_pn * 3) {
+            std::cout << "LEO_ERROR: event classification error: p1, p2 "
+                         "and p3 are "
+                      << p1 << ", " << p2 << ", " << p3 << " at ev. " << ev
+                      << '\n';
+          }
+        }
+      }
+    }
+
+#if 0
+    if (p1 != max_pn) {
+      joint_hist->Fill((static_cast<double>(p1) + p1_cal[0]) / p1_cal[1]);
+      any_update = true;
+      ev_type_count[0]++;
+    } else if (p2 != max_pn) {
+      joint_hist->Fill((static_cast<double>(p2) + p2_cal[0]) / p2_cal[1]);
+      any_update = true;
+      ev_type_count[1]++;
+    } else if (p3 != max_pn) {
+      joint_hist->Fill((static_cast<double>(p3) + p3_cal[0]) / p3_cal[1]);
+      any_update = true;
+      ev_type_count[2]++;
+    }
+#endif
+
+    if (do_debug == true) { // log the info with any update
       std::cout << p1 << '\t' << p2 << '\t' << p3 << '\n';
       n_detections++;
       // std::cout << n_detections << '\n';
@@ -98,21 +178,23 @@ void Lab2hist2(int filepath_option = 0, int do_debug = false) {
       if (n_detections == debug_detections)
         break; // stop detection
 
-      any_update = 0; // reset any_update
+      // any_update = 0; // reset any_update
     }
   }
 
-  std::cout << '\t' << "p1 stops: " << n_events[0] << '\n'
-            << '\t' << "p2 stops: " << n_events[1] << '\n'
-            << '\t' << "p3 stops: " << n_events[2] << '\n';
+  std::cout << '\t' << "no stops: " << ev_type_count[0] << '\n'
+            << '\t' << "p1 stops: " << ev_type_count[1] << '\n'
+            << '\t' << "p2 stops: " << ev_type_count[2] << '\n'
+            << '\t' << "p3 stops: " << ev_type_count[3] << '\n'
+            << '\t' << "yes yes no: " << ev_type_count[4] << '\n';
 
   TCanvas *canvas = new TCanvas("canvas", "Joint histogram", 1280, 720);
   canvas->cd(1);
+  gPad->SetLogy();
+  gPad->SetGrid();
   gStyle->SetPalette(1);
   joint_hist->Draw();
-  // frame1->GetYaxis()->SetLimits(0, 100);
-  // frame1->GetYaxis()->SetTitle("Events/ 3 GeV");
-  // frame1->GetXaxis()->SetTitle("m_{#font[12]{4l}} (GeV)");
+  p3_hist->Draw("same");
   // add proper legend for histograms
   gPad->BuildLegend();
 }
