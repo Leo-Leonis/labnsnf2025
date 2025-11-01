deb4dbda966b36058406846814db6dafd237958a
Leo-Leonis <yeleandro@icloud.com>
2025-11-01 05:02:59 +0100

    cleaned fit system, corrected Q value, changed parameter initial values

--- Lab2ff.cpp
+++ Lab2ff.cpp
@@ -56,18 +56,39 @@ void set_LEO_style() {
   gStyle->SetTitleSize(.05, "Y");
   // gStyle->SetTitleOffset(1.3, "X"); // ROOT bug: "x" does y-axis and
   // viceversa
-  gStyle->SetTitleOffset(1.3, "Y"); // ROOT bug: "x" does y-axis and viceversa
+  // gStyle->SetTitleOffset(1, "Y"); // ROOT bug: "x" does y-axis and viceversa
 }
 
-Double_t full_eq(Double_t *x, Double_t *par) {
-  Float_t xx = x[0];
-  Double_t N = par[0];  // N
-  Double_t t0 = par[0]; // tau_0
-  Double_t R = par[1];  // R
-  Double_t tc = par[2]; // tau_C
-  Double_t b = par[3];  // b
+Double_t fullEq(Double_t const *x, Double_t const *par) {
+  using TMath::Exp;
+  Float_t const t = x[0];
+  Double_t const N = par[0];  // N
+  Double_t const R = par[1];  // R
+  Double_t const b = par[2];  // offset
+  Double_t const t0 = par[3]; // tau_0
+  Double_t const Lc = par[4]; // Lambda_C
+  Double_t const Q = par[5];  // Huff factor
 
-  return N * (TMath::Exp(xx / b) - 1);
+  return N * (Exp(-t / t0) + (1. / R) * Exp(((-t) * (Lc + (Q / t0))))) + b;
+}
+
+Double_t redEq(Double_t const *x, Double_t const *par) {
+  using TMath::Exp;
+  Float_t const t = x[0];
+  Double_t const N = par[0];  // N
+  Double_t const b = par[1];  // offset
+  Double_t const t0 = par[2]; // tau_0
+
+  return N * Exp(-t / t0) + b;
+}
+
+Double_t test_exp(Double_t const *x, Double_t const *par) {
+  using TMath::Exp;
+  Float_t const t = x[0];
+  Double_t const N = par[0];   // N
+  Double_t const tau = par[1]; // tau
+
+  return N * Exp(-t / tau);
 }
 
 void Lab2ff() {
@@ -76,10 +97,14 @@ void Lab2ff() {
   set_LEO_style();
 
   double const R = 1.21;
-  double const lambdaC = 4.4 * pow(10, -6);
+  double const lambdaC = 4.4e-3;
+  double const Q = 0.975;
 
   // histogram that contains all true positive events
   TH1D *tp_h = new TH1D();
+
+  double const range_min = 0;
+  double const range_max = 16500;
 
   TFile *res_file = new TFile("result.root");
   if (res_file->IsZombie()) {
@@ -101,45 +126,37 @@ void Lab2ff() {
   // to the reference guide Full equation, fitting for tau0, tauc/tau_mu^minus,
   // and b
 
-  TF1 *fullForm = new TF1(
-      "fullForm", "[N]*exp(-x/[tau0])*(1+exp(-x/[tauC])/[R])+[b]", 0, 17000);
+  TF1 *fullForm = new TF1("fullForm", fullEq, range_min, range_max, 6);
 
-  std::cout << fullForm->GetParNumber("tauC") << '\n';
-  std::cout << fullForm->GetParNumber("R") << '\n';
-  std::cout << fullForm->GetParNumber("N") << '\n';
-  std::cout << fullForm->GetParNumber("tau0") << '\n';
-  std::cout << fullForm->GetParNumber("b") << "\n" << '\n';
+  ///////                0    1    2     3        4       5
+  fullForm->SetParNames("N", "R", "b", "tau0", "labdaC", "Q");
+  ///////                0    1    2     3        4       5
 
-  // N
   // fullForm->FixParameter(0, tp_count);
-  // R
   fullForm->FixParameter(1, R);
-  // b
   // fullForm->SetParLimits(2,0,16000);
   fullForm->SetParameter(2, 50);
-  // tauC
-  // fullForm->SetParLimits(4,0,16000);
-  fullForm->SetParameter(4, 100);
-  // tau0
   // fullForm->SetParLimits(3,0,16000);
   fullForm->SetParameter(3, 800);
+  // fullForm->SetParLimits(4,0,16000);
+  // fullForm->SetParameter(4, 0);
+  fullForm->SetParLimits(4, 1e-3, 1e-2);
+  fullForm->FixParameter(5, Q);
+  // fullForm->SetParameter(5, 1);
+  // fullForm->SetParLimits(5, 0.7, 1);
 
   // Reduced equation, fitting only for tau0 and b.
-  TF1 *redForm = new TF1("redForm", "[N]*exp(-x/[tau])+[b]", 4000, 8000);
-  std::cout << redForm->GetParNumber("t0") << '\n';
-  std::cout << redForm->GetParNumber("N") << '\n';
-  std::cout << redForm->GetParNumber("tau") << '\n';
-  std::cout << redForm->GetParNumber("b") << '\n';
-  // N
-  // redForm->FixParameter(0, tp_count);
-  // tau0
-  // redForm->SetParLimits(3,0,1000);
-  redForm->SetParameter(3, 200);
-  // b
+  TF1 *redForm = new TF1("redForm", redEq, range_min, range_max, 3);
+
+  ///////                0     1     2
+  redForm->SetParNames("N_r", "b", "t0_r");
+  ///////                0     1     2
+
+  redForm->SetParameter(0, 4000);
   // redForm->SetParLimits(1,0,1000);
   redForm->SetParameter(1, 50);
-  // t0
-  redForm->FixParameter(2, 4);
+  // redForm->FixParameter(2, 4);
+  redForm->SetParameter(2, 2000);
 
   fullForm->SetLineColor(kRed);
   redForm->SetLineColor(kCyan);
@@ -148,14 +165,15 @@ void Lab2ff() {
   // tp_h->SetFillColor(kPink);
   tp_h->SetMarkerStyle(kFullCircle);
   tp_h->SetMarkerColor(kBlue);
-  TFitResultPtr fullFit = tp_h->Fit(fullForm, "RS", "");
-  TFitResultPtr redFit = tp_h->Fit(redForm, "RS+", "");
+  TFitResultPtr fullFit = tp_h->Fit(fullForm, "SR", "");
+  TFitResultPtr redFit = tp_h->Fit(redForm, "SR+", "", 2000, 8000);
 
-  TF1 *testForm = new TF1("testForm", "[0]*exp(-x/[1])", 1000, 17000);
+  TF1 *testForm = new TF1("testForm", test_exp, range_min, range_max, 2);
   testForm->SetLineColor(kViolet);
-  testForm->FixParameter(0, tp_count);
-  testForm->SetParameter(1, 10000);
-  TFitResultPtr testFit = tp_h->Fit(testForm, "RS+", "");
+  testForm->SetParNames("N_t", "tau");
+  testForm->SetParameter(0, 4000);
+  testForm->SetParameter(1, 2000);
+  TFitResultPtr testFit = tp_h->Fit(testForm, "SR+", "", 1000, 16500);
 
   canvas1->SetLogy();
 
@@ -167,15 +185,13 @@ void Lab2ff() {
   // add TLatex symbols for parameters
   // Fix cutoff for redform, the equation that ignores muon capture
   // Check redForm equation
-  std::cout << "Chi^2:" << '\n';
+  std::cout << '\n' << "Chi^2:" << '\n';
   std::cout << '\t' << "Full fit: " << fullForm->GetChisquare() << '\n';
   std::cout << '\t' << "Reduced fit: " << redForm->GetChisquare() << '\n';
-  double tau0 = fullForm->GetParameter("tau0");
-  double tauC = fullForm->GetParameter("tauC");
-  // double Q = (1 / (tau0 + tauC) - lambdaC) * tau0;
-  std::cout << "Huff Factor (Q): " << '\n';
-  // std::cout << Q << '\n';
-  std::cout << (1 / (tau0 + tauC) - lambdaC) * tau0 << '\n';
+  // double tau0 = fullForm->GetParameter("tau0");
+  // double tauC = fullForm->GetParameter("tauC");
+  // std::cout << "Huff Factor (Q): " << '\n';
+  // std::cout << (1 / (tau0 + tauC) - lambdaC) * tau0 << '\n';
 
   canvas1->Print("graphs/Lab2ff/Lab2ff.pdf");
 }
